02753345a39e77b4dcc0b0d5835a89a0dd2a5d1c
marcelo__nm <marcelomascarenhas73@gmail.com>
2024-12-03 14:50:19 -0300

    hello word2

--- src/varios_códigos/teste.java
+++ src/varios_códigos/teste.java
@@ -1,5 +1,8 @@
 package varios_códigos;
 
 public class teste {
-
+    // Método principal que será executado ao rodar o programa
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Este é um exemplo de código em Java.");
+    }
 }
